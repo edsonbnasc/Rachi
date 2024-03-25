3363f23dd51ecc8cfa4f24e04ee14fe419a0effc
Edson Batista <edsonbnasc@uninove.edu.br>
2024-03-25 18:24:57 -0300

    correcao js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,6 @@
 const headerlinks = document.querySelector(".nav_bar")
 document.getElementById("handleNavbar").addEventListener("click",()=>{headerlinks.classList.toggle("open")})
+
+(function () {
+    headerlinks.classList.remove("open");
+}());
